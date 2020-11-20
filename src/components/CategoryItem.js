6a8ead3941db3f8763d395cfcc277adcd632d931
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-11-20 17:53:16 +0600

    body category items styled

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './CategoryItem.css'
+import './CategoryItem.css';
 
 const CategoryItem = (props) => {
     return (
-        <div>
-            {props.name} <br/>
-            {props.productStock} <br/> 
-            {props.description} <br/>
-            <img src={props.image} alt=""/> <br/>
+        <div className="categoryItemContainer">
+        <div className="categoryItem">
+            <div className="categoryItemImage"><img src={props.image} alt=""/></div> 
+            <div className="categoryItemName"><div>{props.name}</div></div>
+            <div className="categoryItemStock">({props.productStock})</div>
+            <div className="categoryItemDescription">{props.description}</div>
+        </div>
         </div>
     )
 }
