586c8c1eaf991f6330752b5005f6d9cdcb12393b
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-11-26 19:30:48 +0600

    postAd comp initial ui

--- src/components/PostAd.js
+++ src/components/PostAd.js
@@ -8,7 +8,44 @@ const PostAd = () => {
     return (
         <div className="postAdContainer">
             <div className="postAdBody">
-                <h4>Welcome {userName}! Let's post an ad.</h4>
+                <div className="postAdHeader">
+                    <h4 style={{marginTop: '0px', marginBottom: '12px'}}>Welcome {userName}! Let's post an ad.</h4>
+                    <span style={{marginTop: '0px'}}>Choose any option below.</span>
+                </div>
+
+                <div className="postAdMain">
+                    <div className="postAdBodyColumn">
+                        <div className="postAdBodyColumnHeading"
+                        style={{borderTop: '1px solid rgb(230, 230, 230)'}}
+                        >
+                            <img src="cash.svg" alt=""/>
+                            <span>Sell something</span>
+                        </div>
+                        <div><a href="#">Sell an item,property or service</a></div>
+                        <div><a href="#">Offer a property for rent</a></div>
+                    </div>
+                    <div className="postAdBodyColumn">
+                        <div className="postAdBodyColumnHeading"
+                        style={{borderTop: '1px solid rgb(230, 230, 230)'}}
+                        >
+                            <img src="briefcase.svg" alt=""/>
+                            <span>Sell something</span>
+                        </div>
+                        <div><a href="#">Sell an item,property or service</a></div>
+                        <div><a href="#">Offer a property for rent</a></div>
+                    </div>
+                    <div className="postAdBodyColumn">
+                        <div className="postAdBodyColumnHeading"
+                        style={{borderTop: '1px solid rgb(230, 230, 230)'}}
+                        >
+                            <img src="earthLook.svg" alt=""/>
+                            <span>Sell something</span>
+                        </div>
+                        <div><a href="#">Sell an item,property or service</a></div>
+                        <div><a href="#">Offer a property for rent</a></div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
